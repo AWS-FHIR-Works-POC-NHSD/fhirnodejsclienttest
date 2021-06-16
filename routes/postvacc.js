a428b20a02c138610763396693e5e84bbe3ff80a
Graham Dodsworth <31283488+grahamd99@users.noreply.github.com>
2021-06-16 11:11:46 +0100

    Update postvacc.js

--- routes/postvacc.js
+++ routes/postvacc.js
@@ -28,20 +28,15 @@ router.post('/', function(req, res, next) {
  var expirationDate   = req.body.expirydate;
  var lotNumber        = req.body.batchnumber;
 
-if (vaccineProcedure == "covdose1")
+if (vaccineProcedure == "dose1")
  { 
   vaccineProcedureCode = "1324681000000101";
   vaccineProcedureDescription = "Administration of first dose of SARS-CoV-2 (severe acute respiratory syndrome coronavirus 2) vaccine";
 }
-if (vaccineProcedure == "covdose2")
+if (vaccineProcedure == "dose2")
  { 
   vaccineProcedureCode = "1324691000000104";
   vaccineProcedureDescription = "Administration of second dose of SARS-CoV-2 (severe acute respiratory syndrome coronavirus 2) vaccine";
-}
-if (vaccineProcedure == "hpvdose")
- { 
-  vaccineProcedureCode = "734152003";
-  vaccineProcedureDescription = "Administration of vaccine product containing only Human papillomavirus 6, 11, 16 and 18 antigens";
 }
 
  var vaccineProduct = req.body.vaccine;
@@ -61,11 +56,6 @@ if (vaccineProcedure == "hpvdose")
   vaccineProductCode = "39326911000001101";
   vaccineProductDescription = "COVID-19 mRNA (nucleoside modified) Vaccine Moderna 0.1mg/0.5mL dose dispersion for injection multidose vials (Moderna, Inc)";
 }
- if (vaccineProduct == "gardasil")
- { 
-  vaccineProductCode = "10880211000001104";
-  vaccineProductDescription = "Gardasil vaccine suspension for injection 0.5ml pre-filled syringes (Merck Sharp & Dohme (UK) Ltd)";
-}
 
 //var POCidentifier = uuidv4();
 
@@ -78,127 +68,124 @@ if (vaccineProcedure == "hpvdose")
 axios.post('/Immunization', 
 {
   "resourceType": "Immunization",
-  "meta": {
-       "profile":  [
-          "https://fhir.nhs.uk/StructureDefinition/NHSDigital-Immunization"
-        ]
-   },
-  "extension": [ {
-        "url": "https://fhir.hl7.org.uk/StructureDefinition/Extension-UKCore-VaccinationProcedure",
-        "valueCodeableConcept": {
-          "coding": [ {
-            "system": "http://snomed.info/sct",
-            "code": vaccineProcedureCode,
-            "display": vaccineProcedureDescription
-          } ]
-        }
-      } ],
-  "identifier": [
-    {
-      "system": "https://supplierABC/identifiers/vacc",
-      "value": POCidentifier
-    }
+  "id": "696f13f9-a246-4c28-98be-cd979b86bfe2",
+  "extension":  [
+      {
+          "url": "https://fhir.hl7.org.uk/StructureDefinition/Extension-UKCore-VaccinationProcedure",
+          "valueCodeableConcept": {
+              "coding":  [
+                  {
+                      "system": "http://snomed.info/sct",
+                      "code": vaccineProcedureCode ,  
+                      "display": vaccineProcedureDescription
+                  }
+              ]
+          }
+      }
+  ],
+  "identifier":  [
+      {
+          "system": "https://tools.ietf.org/html/rfc4122",
+          "value": POCidentifier  
+      }
   ],
   "status": "completed",
   "vaccineCode": {
-  "coding": [
-   {
-     "system": "http://snomed.info/sct",
-     "code": vaccineProductCode,
-     "display": vaccineProductDescription
-   }
-   ]
+      "coding":  [
+          {
+              "system": "http://snomed.info/sct",
+              "code": vaccineProductCode ,
+              "display": vaccineProductDescription 
+          }
+      ]
   },
   "patient": {
-    "reference": "Patient/1953684",
-    "type": "Patient",
-    "identifier": {
-      "system": "https://fhir.nhs.uk/Id/nhs-number",
-      "value": nhsNumber
+      "reference": "Patient/1953684",
+      "type": "Patient",
+      "identifier": {
+          "system": "https://fhir.nhs.uk/Id/nhs-number",
+          "value": nhsNumber 
       }
   },
-  "encounter": {
-    "reference": "Encounter/example"
-  },
-  "occurrenceDateTime": date,
+  "occurrenceDateTime": date, 
+  "recorded": "2020-12-23",
   "primarySource": true,
-  "location": {
-    "reference": "Location/1"
-  },
   "manufacturer": {
-    "reference": "Organization/hl7"
+      "display": "DREAMLAND Pharmaceuticals Ltd"
   },
-  "lotNumber": lotNumber,
-  "expirationDate": expirationDate,
+  "lotNumber": lotNumber ,
+  "expirationDate": expirationDate ,
   "site": {
-    "coding": [
-      {
-        "system": "http://snomed.info/sct",
-        "code": "368208006",
-        "display": "Left upper arm structure (body structure)"
-      }
-    ]
+      "coding":  [
+          {
+              "system": "http://snomed.info/sct",
+              "code": "368208006",
+              "display": "Left upper arm structure (body structure)"
+          }
+      ]
   },
   "route": {
-    "coding": [
-      {
-        "system": "http://snomed.info/sct",
-        "code": "78421000",
-        "display": "Intramuscular route (qualifier value)"
-      }
-    ]
+      "coding":  [
+          {
+              "system": "http://snomed.info/sct",
+              "code": "78421000",
+              "display": "Intramuscular route (qualifier value)"
+          }
+      ]
   },
   "doseQuantity": {
-    "value": 0.5,
-    "unit": "Millilitre",
-    "system": "http://snomed.info/sct",
-    "code": "258773002"
+      "value": 1,
+      "unit": "pre-filled disposable injection",
+      "system": "http://snomed.info/sct",
+      "code": "3318611000001100"
   },
-  "performer": [
-    {
-      "function": {
-        "coding": [
-          {
-            "system": "http://terminology.hl7.org/CodeSystem/v2-0443",
-            "code": "OP"
+  "performer":  [
+      {
+          "actor": {
+              "type": "Practitioner",
+              "identifier": {
+                  "system": "https://fhir.hl7.org.uk/Id/nmc-number",
+                  "value": "5566789"
+              },
+              "display": "HOLDING, Rafferty"
           }
-        ]
       },
-      "actor": {
-        "reference": "Practitioner/example"
-      }
-    },
-    {
-      "function": {
-        "coding": [
-          {
-            "system": "http://terminology.hl7.org/CodeSystem/v2-0443",
-            "code": "AP"
+      {
+          "function": {
+              "coding":  [
+                  {
+                      "system": "http://terminology.hl7.org/CodeSystem/v2-0443",
+                      "code": "AP",
+                      "display": "Administering Provider"
+                  }
+              ]
+          },
+          "actor": {
+              "type": "Organization",
+              "identifier": {
+                  "system": "https://fhir.nhs.uk/Id/ods-organization-code",
+                  "value": "RX809"
+              }
           }
-        ]
-      },
-      "actor": {
-        "reference": "Practitioner/example"
       }
-    }
   ],
-  "note": [
-    {
-      "text": "Notes on adminstration of vaccine"
-    }
+  "reasonCode":  [
+      {
+          "coding":  [
+              {
+                  "system": "http://snomed.info/sct",
+                  "code": "443684005",
+                  "display": "Disease outbreak (event)"
+              }
+          ]
+      }
   ],
-  "reasonCode": [
-    {
-      "coding": [
-        {
-          "system": "http://snomed.info/sct",
-          "code": "443684005",
-          "display": "Disease outbreak (event)"
-        }
-      ]
-    }
-    ]
-   }
+  "protocolApplied":  [
+      {
+          "doseNumberPositiveInt": 1
+      }
+  ]
+}
   )
   .then(function (response) {
     //console.log(response);
