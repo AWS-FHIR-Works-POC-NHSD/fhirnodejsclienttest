9b4b9221debcad3e801a31949b5c8f47c2c05a0d
tim.coates <tim.coates@gmail.com>
2022-10-06 10:24:05 +0100

    Aded tests for delete route to improve the visibility of test structure / layout.

--- routes/test/app.test.js
+++ routes/test/app.test.js
@@ -1,10 +1,9 @@
 const request = require("supertest");
 const app = require("../../app");
 
-
 describe("Testing the app responses", () => {
 
-	describe("Test the root (/) path", () => {
+	describe("Test the root (/) route", () => {
 
 		test("It should respond (200 OK) to the GET method", async () => {
 			const response = await request(app).get("/");
@@ -29,6 +28,36 @@ describe("Testing the app responses", () => {
 
 		test("The response from the GET method should NOT include the Page Heading: <h2>Patient page<", async () => {
 			const response = await request(app).get("/");
+			expect(response.text).not.toMatch(/<h2>Patient page</i);
+		});
+
+	});
+
+	describe("Test the delete (/delete) route", () => {
+
+		test("It should respond (200 OK) to the GET method", async () => {
+			const response = await request(app).get("/delete");
+			expect(response.statusCode).toBe(200);
+		});
+
+
+		test("It should respond (404 Not Found) to the POST method", async () => {
+			const response = await request(app).post("/delete");
+			expect(response.statusCode).toBe(404);
+		});
+
+		test("The response from the GET method should include the Page Title: <title>EJS demo app<", async () => {
+			const response = await request(app).get("/delete");
+			expect(response.text).toMatch(/<title>EJS demo app</i);
+		});
+
+		test("The response from the GET method should include the Page Heading: <H2>DELETE an existing vaccination record<", async () => {
+			const response = await request(app).get("/delete");
+			expect(response.text).toMatch(/<H2>DELETE an existing vaccination record</i);
+		});
+
+		test("The response from the GET method should NOT include the Page Heading: <h2>Patient page<", async () => {
+			const response = await request(app).get("/delete");
 			expect(response.text).not.toMatch(/<h2>Patient page</i);
 		});
 
